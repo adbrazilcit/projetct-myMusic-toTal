57f25151f47e9ef15f9d00124b8801692aded44a
devandressa <andressas@ciandt.com>
2022-07-12 16:52:47 -0300

    metodos que contém find em repository do package infra e domain

--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/domain/ports/repository/MusicaRepositoryPort.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/domain/ports/repository/MusicaRepositoryPort.java
@@ -1,5 +1,19 @@
 package com.ciandt.summit.bootcamp2022.domain.ports.repository;
 
+import com.ciandt.summit.bootcamp2022.domain.Musica;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface MusicaRepositoryPort {
+
+
+    Optional<Musica> findMusicById(String id);
+
+    List<Musica> findAll();
+
+    List<Musica> findByNomeStartingWith(String nome);
+
+    List<Musica> findByArtistaStartingWith(String nome);
 
 }
--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/infrastructure/adapters/repository/SpringMusicaRepository.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/infrastructure/adapters/repository/SpringMusicaRepository.java
@@ -1,7 +1,22 @@
 package com.ciandt.summit.bootcamp2022.infrastructure.adapters.repository;
 
+import com.ciandt.summit.bootcamp2022.infrastructure.adapters.entities.MusicaEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
-public interface SpringMusicaRepository {
+public interface SpringMusicaRepository extends JpaRepository<MusicaEntity, String> {
+
+    Optional<MusicaEntity> findMusicById(String id);
+
+    List<MusicaEntity> findAll();
+
+    List<MusicaEntity> findByNomeStartingWith(String nome);
+
+    List<MusicaEntity> findByArtistaStartingWith(String nome);
 }
