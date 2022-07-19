d7e692abcb6445e290698b815a6491ca1095ca68
devandressa <andressas@ciandt.com>
2022-07-19 14:24:57 -0300

    preHandle request

--- MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/config/Interceptor.java
+++ MyMusic/src/main/java/com/ciandt/summit/bootcamp2022/config/Interceptor.java
@@ -1,41 +1,42 @@
 package com.ciandt.summit.bootcamp2022.config;
 
+
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
+
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+
 import java.net.http.HttpResponse;
+import java.nio.file.Path;
 import java.util.logging.Logger;
 
 
 @Component
-public class Interceptor  implements HandlerInterceptor {
+public class Interceptor implements HandlerInterceptor {
 
-    private static final Logger log =Logger.getLogger(Interceptor.class.getName());
+    private static final Logger log = Logger.getLogger(Interceptor.class.getName());
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         // create a client
         HttpClient client = HttpClient.newHttpClient();
 
-      // create a request
+        // create a request
         HttpRequest request1 = HttpRequest.newBuilder(
                         URI.create("http://localhost:8081/api/v1/token/authorizer"))
                 .header("Content-Type", "application/json")
-                .POST(HttpRequest.BodyPublishers.ofString(" {\n" +
-                        " \"data\": {\n" +
-                        "        \"name\": \"fulano\",\n" +
-                        "        \"token\": \"4EaSe47fPycRFVZbMMByOwMcx1YR6zVRyaouPDzR03Qpr7/4hhcAXpq0CttmB9lk\"\n" +
-                        "    }\n" +
-                        " }"))
+                .POST(HttpRequest.BodyPublishers.ofFile(Path.of("./src/main/resources/data/token.json")))
                 .build();
 
-//        System.out.println(client.send(request1,  );
+        log.info("Request: " + request1.toString());
 
+        // send the request
+        client.send(request1, HttpResponse.BodyHandlers.ofString());
 
         log.info("Requisição interceptada: " + request.getHeader("Authorization"));
         return true;
